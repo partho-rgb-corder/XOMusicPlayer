51a21f5b3146cd8f3753ac33cc4b99bc774b4288
parthoda <py556624@gmail.com>
2026-08-16 23:01:21 +0600

    Update SettingsScreen.kt

--- XOPlayer/app/src/main/java/com/xoplayer/app/ui/settings/SettingsScreen.kt
+++ XOPlayer/app/src/main/java/com/xoplayer/app/ui/settings/SettingsScreen.kt
@@ -1,7 +1,6 @@
 package com.xoplayer.app.ui.settings
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
