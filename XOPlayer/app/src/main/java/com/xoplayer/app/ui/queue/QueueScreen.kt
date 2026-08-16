8335c3c7f10c8f77a8958e092b5e94401d1e92db
parthoda <py556624@gmail.com>
2026-08-16 23:00:53 +0600

    Update QueueScreen.kt

--- XOPlayer/app/src/main/java/com/xoplayer/app/ui/queue/QueueScreen.kt
+++ XOPlayer/app/src/main/java/com/xoplayer/app/ui/queue/QueueScreen.kt
@@ -2,7 +2,6 @@ package com.xoplayer.app.ui.queue
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
